416a58817866195cfb259d081f7d211c75e8a162
Mateusz Wuzik <wuzikm@gmail.com>
2025-10-05 11:32:13 +0200

    dash

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,11 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { useTranslation } from 'react-i18next'
+import { createFileRoute } from '@tanstack/react-router';
+import { useTranslation } from 'react-i18next';
 
 export const Route = createFileRoute('/about')({
   component: About,
-})
+});
 
 function About() {
-  const { t } = useTranslation()
-  return <div className='p-2'>{t('hello')}</div>
+  const { t } = useTranslation();
+  return <div className="p-2">{t('hello')}</div>;
 }
